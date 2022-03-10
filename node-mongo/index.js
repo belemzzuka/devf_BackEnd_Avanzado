c68012341573b9e86b02002ce75e762b5db110b4
Belem Garrido <belem.garrido@gmail.com>
2022-03-09 19:54:09 -0600

    node-mongo project. Connecting the database, creating a new db with a collection

---
+++ node-mongo/index.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = 'mongodb+srv://belemzzuka:qwerty123@cluster0.viuop.mongodb.net/chat?retryWrites=true&w=majority'
+
+async function main(){
+    await mongoose.connect(MONGO_URI)
+}
+
+function createMessage() {
+    // crear una entidad en la base de datos
+    const messageSchema = new mongoose.Schema({
+        content: String,
+        sender: String,
+        read: Boolean
+    });
+
+    messageSchema.methods.send = function send() {
+        console.log(`El mensaje "${this.content}" fue enviado.`);
+    }
+
+    const Message = mongoose.model('Message', messageSchema) // el modelo recibe el esquema del mensaje
+
+    const message = new Message({ // iniciamos una entidad con ese modelo con los valores que queremos que tenga
+        content: 'Este es el primer mensaje',
+        sender: 'Misael Calvillo',
+        read: false
+     })
+
+     return message.save(); //mandarlo a la base de datos de mongo. Save es una promesa de mongoose
+}
+
+main()
+.then(() => {
+    console.log('La conexión es exitosa')
+    createMessage()
+    .then((message) => message.send())
+    .catch(err => console.log(err));
+})
+.catch((err) => {
+    console.log(err)
+});
