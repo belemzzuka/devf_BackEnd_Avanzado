0eca24f0a3ca90102ffa395d3bb1fc4412d738ac
Belem Garrido <belem.garrido@gmail.com>
2022-03-10 19:40:32 -0600

    middleware para error handling

--- tareas/backend/controllers/tareaControllers.js
+++ tareas/backend/controllers/tareaControllers.js
@@ -1,8 +1,18 @@
+
+//MVC - CONTROLLER
+
 const getTareas = (req, res) => {
     res.status(200).json({ message: 'Obtener Tareas'})
 }
 
 const postTareas = (req, res) => {
+
+    if(!req.body.text){
+        // res.status(400).json({ message: 'Es obligatorio agregar una tarea' }) // Manda un mensaje en la consola
+        res.status(400)
+        throw new Error('Es obligatorio agregar una tarea')
+    }
+
     res.status(200).json({ message: 'Crear una tarea'})
 }
 
--- tareas/backend/server.js
+++ tareas/backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config(); //traer la config de .env
+const { errorHandler } = require('./middlewares/errorMiddleware')
 const port = process.env.PORT || 5000;
 
 const app = express(); // crear la app
+
+app.use(express.json()) // middleware para que la aplicacion tenga acceso a los metodos de json express
+app.use(express.urlencoded({ extended: false })) //podamos usar el urlencoded para leer el body
+app.use(errorHandler) // consumir el error handler
 
 app.use('/api/tareas', require('./routes/tareaRoutes'))
 
