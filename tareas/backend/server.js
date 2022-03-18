36bc366fd098ff2cad8aefa1d70d79cd3819af04
Belem Garrido <belem.garrido@gmail.com>
2022-03-17 22:16:57 -0600

    added CORS library

--- tareas/backend/server.js
+++ tareas/backend/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const dotenv = require('dotenv').config(); //traer la config de .env
+const cors = require('cors');
 const { errorHandler } = require('./middlewares/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
@@ -9,6 +10,8 @@ const port = process.env.PORT || 5000;
 connectDB() //conectar a la db con lo que trae del archivo config/db
 
 const app = express(); // crear la app
+
+app.use(cors()); //ejecutar cors
 
 app.use(express.json()) // middleware para que la aplicacion tenga acceso a los metodos de json express
 app.use(express.urlencoded({ extended: false })) //podamos usar el urlencoded para leer el body
