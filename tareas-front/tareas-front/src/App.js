88c89a198a62669d470d40b3bc048d648cbf97ca
Belem Garrido <belem.garrido@gmail.com>
2022-03-22 21:37:19 -0600

    add and delete tasks from FrontEnd using concat() and filter()

--- tareas-front/tareas-front/src/App.js
+++ tareas-front/tareas-front/src/App.js
@@ -1,31 +1,72 @@
 import './App.css';
-import { fetchTasks } from './api';
+import { fetchTasks, createTasks, deleteTasks } from './api';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [value, setValue] = useState("");
-  //console.log("App se renderiza");
-  //console.log(value)
+  const [taskText, setTaskText] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [loader,setLoader] = useState(false); //agregar la palabra loading
+
   console.log(tasks)
 
   useEffect(() => {
-    fetchTasks()
+    setLoader(true); //inicia como true para que veas que algo está cargando
+    const timeoutId = setTimeout(() => {
+      console.log("Este proceso pasa despues de 5 segundos")
+      fetchTasks()
+      .then((res) => {
+        setTasks(res.data)
+        setLoader(false) //lo apagas cuando ya desplegó el contenido
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }, 5000)
+
+    return () => {
+      clearTimeout(timeoutId) //este return se ejecuta hasta que 
+    }
+
+  }, [])
+
+// ## OPCIONES PARA AGREGAR TAREA (POST)
+//Opt1 - Bajar todas las tareas otra vez (si el backend no responde la tarea) (MENOS EFICIENTE)
+
+//Opt2 - Agregar la tarea recien creada a la lista (MAS EFICIENTE)
+
+//Opt3 - (PUNTO MEDIO DE EFICIENCIA)
+  //1. Agregar la tarea a la lista local. (sin saber si la tarea se agrega de forma exitosa)
+  //2. Hacer post y esperar respuesta
+  //3. Dependiendo si exitoso ese fetch, actualizo la interfaz
+    //a. Si el fetch es exitoso, agrego id.
+    //b. Elimino el elemento.
+  const addTask = () => {
+    createTasks(taskText)
     .then((res) => {
-      setTasks(res.data)
-      console.log(res.data)
+      const createdTask = res.data;
+      setTasks(tasks.concat(createdTask))
+      setTaskText("")
     })
     .catch((err) => {
       console.log(err)
     })
-  }, [])
-
-  const addTask = () => {
-    console.log("Agrega la tarea", value)
+    /*
     setTasks(tasks.concat({ //diferencia entre push y concat: Concat crea una copia exacta y la deja en el elemento, push devuelve el length del array.
-      _id: "6233f2c42bff33495cf6c02a" + Math.floor(Math.random() * 10),
-      text: value,
-    }))
+      text: taskText,
+    }))*/
+  }
+
+  // ## ELIMINAR TAREAS
+  const deleteTask = (id) => {
+    deleteTasks(id)
+    .then((res) => {
+      const updatedTasks = tasks.filter( task => task._id !== id )
+      console.log("UPDATED TASKS", updatedTasks)
+      setTasks(updatedTasks)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
   }
 
   return (
@@ -36,26 +77,26 @@ function App() {
           <input 
             type="text" 
             className="task-input__text"
-            value={value}
+            value={taskText}
             placeholder = "Ingresa la tarea aquí"
-            onChange={(event) => {
-              setValue(event.target.value)
-            }} />
+            onChange={(event) => setTaskText(event.target.value)} />
         </div>
           <button 
-            onClick={ addTask } 
+            onClick={ () => { addTask() }
+               } 
             className="task-input__btn">
               Ingresar tarea
           </button>
       </div>
-
+        {loader && (<p style={{ color: 'white' }}>Loading ...</p>)}
         {tasks.map((task) => {
           return (
             <div key={task._id} className="task">
               <p>{task.text}</p>
+              <span className="task__delete" onClick={ () => deleteTask(task._id) } >X</span>
             </div>
           )
-        })}
+        }).reverse()}
         
       </header>
     </div>
--- tareas-front/tareas-front/src/api/index.js
+++ tareas-front/tareas-front/src/api/index.js
@@ -3,7 +3,27 @@ import axios from 'axios';
 export const fetchTasks = (token) => {
     return axios.get('http://localhost:5000/api/tareas', { 
         headers: {
-            'Authorization': `Bearer <token>`,
+            'Authorization': `Bearer ${process.env.REACT_APP_MONGO_DB_TOKEN}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+}
+
+export const createTasks = (text) => {
+    return axios.post('http://localhost:5000/api/tareas',{ 
+        text: text
+    }, {
+        headers: {
+            'Authorization': `Bearer ${process.env.REACT_APP_MONGO_DB_TOKEN}`,
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+export const deleteTasks = (id) => {
+    return axios.delete(`http://localhost:5000/api/tareas/${id}`,{
+        headers: {
+            'Authorization': `Bearer ${process.env.REACT_APP_MONGO_DB_TOKEN}`,
             'Content-Type': 'application/x-www-form-urlencoded'
         }
     })
